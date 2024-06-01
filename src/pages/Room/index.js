1a7b3a04b5a0f478bae4e288b678289ba9e72812
Gumerov-Vadim <95773619+Gumerov-Vadim@users.noreply.github.com>
2024-06-02 03:12:17 +0500

    changed togglecamera method v1

--- src/pages/Room/index.js
+++ src/pages/Room/index.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router';
 import useWebRTC, { LOCAL_VIDEO } from '../../hooks/useWebRTC';
 import socket from '../../socket';
+import { useState } from 'react';
 const ACTIONS = require('../../socket/actions');
 
 function layout(clientsNumber = 1) {
@@ -28,15 +29,23 @@ export default function Room() {
     const toggleMic = (clientID) => {
     socket.emit(ACTIONS.MODERATOR_ACTION, { targetClientID: clientID, action: 'toggleMic' });
   };
-
+  const [disabledvideo, setdisabledvideo] = useState([]);
   const toggleCamera = (clientID) => {
+    disabledvideo.includes(clientID)? setdisabledvideo(disabledvideo.filter(id => id !== clientID)):setdisabledvideo([...disabledvideo, clientID]);
     socket.emit(ACTIONS.MODERATOR_ACTION, { targetClientID: clientID, action: 'toggleCamera' });
   };
 
+  const isVideoDisabled = (clientID) => {
+    return disabledvideo.includes(clientID);
+  }
+  const isMuted = (clientID) => {
+    return clientID === LOCAL_VIDEO;
+  }
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap', height: '100vh' }}>
       {clients.map((clientID, index) => {
         // const test = false; как можно выключать вебку
+        if(!isVideoDisabled(clientID)){
         return (
         <div key={clientID} style={videoLayout[index]} id={clientID}>
           <video
@@ -45,10 +54,10 @@ export default function Room() {
             ref={instance => provideMediaRef(clientID, instance)}
             autoPlay
             playsInline
-            muted={clientID === LOCAL_VIDEO}
+            muted={isMuted(clientID)}
             style={{
               // display: test ?'none':'block' как можно выключать вебку
-              displat: 'block'
+              display: 'block'
             }}
           />
            {isModerator && clientID !== LOCAL_VIDEO && (
@@ -59,7 +68,34 @@ export default function Room() {
           )}
         </div>
       );
-      })}
+    }
+    else{
+      
+      return (
+        <div key={clientID} style={videoLayout[index]} id={clientID}>
+          <audio
+            width='100%'
+            height='100%'
+            ref={instance => provideMediaRef(clientID, instance)}
+            autoPlay
+            playsInline
+            muted={isMuted(clientID)}
+            style={{
+              // display: test ?'none':'block' как можно выключать вебку
+              display: 'block'
+            }}
+          />
+           {isModerator && clientID !== LOCAL_VIDEO && (
+            <div>
+              <button onClick={() => toggleMic(clientID)}>Toggle Mic</button>
+              <button onClick={() => toggleCamera(clientID)}>Toggle Camera</button>
+            </div>
+          )}
+        </div>
+      );
+    }
+      })
+    }
     </div>
   );
 }
